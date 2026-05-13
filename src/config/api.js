510828ad9feaadd2ad0f2cc58787301296e9c276
Horizon876 <szymek.kotula@gmail.com>
2026-05-13 15:37:42 +0200

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+export const API_URL = import.meta.env.PROD ? 'http://ojyk2drw0xhoz8ihrrcujo5p.51.83.197.19.sslip.io' : 'http://localhost:3000';
